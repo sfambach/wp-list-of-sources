83bf43e3a415a23ab785a64421f84a7db527ea15
sfambach <spam1@bobbelsche.de>
2026-07-21 23:28:45 +0200

    initial upload

--- wp-list-of-sources.php
+++ wp-list-of-sources.php
@@ -121,6 +121,146 @@ function wpls_render_sources_table( $attributes, $content ) {
     $wrapper_class_str = implode( ' ', array_map( 'sanitize_html_class', $wrapper_classes ) );
 
     if ( $is_template_preview ) {
+        return wpls_render_template_dummy_preview($wrapper_class_str, $title_sources, $title_images, $title_tables, $heading_tag, $table_style_class, $display_format);
+    }
+
+    $cache_key = 'wpls_cache_' . $post_id . '_' . md5(serialize($attributes));
+    $cached_output = get_transient($cache_key);
+    if ( $cached_output !== false ) { return $cached_output; }
+
+    $html = $post->post_content;
+    if (empty(trim($html))) { return '<p style="font-style:italic; color:#666;">' . esc_html__( 'No content found to analyze.', 'wp-list-of-sources' ) . '</p>'; }
+
+    // BESSERER FIX: Vorherigen Zustand speichern & Interne LibXML-Fehler aktivieren
+    $previous_error_state = libxml_use_internal_errors(true);
+    
+    $dom = new DOMDocument();
+    
+    // HTML mit HTML5-Kompatibilitätsflags laden, um Gutenberg-Kommentarfehler abzufangen
+    $dom->loadHTML(
+        '<?xml encoding="utf-8" ?>' . trim($html), 
+        LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD
+    );
+    
+    // Fehlerpuffer sofort leeren und alten Zustand wiederherstellen
+    libxml_clear_errors();
+    libxml_use_internal_errors($previous_error_state);
+
+    $links_data = []; $images_data = []; $tables_data = [];
+
+    $links = $dom->getElementsByTagName('a');
+    foreach ($links as $link) {
+        $url = $link->getAttribute('href'); 
+        $title = $link->getAttribute('title'); 
+        $text = trim($link->textContent);
+        
+        // leere URLs und Anker entfernen
+        if (empty($url) || strpos($url, '#') === 0) continue;
+        
+        if ( true === WP_DEBUG ) {
+            echo ' '.$url.' ';
+        }
+        
+        // GEFIXT: Fehlendes $ bei $title ergänzt
+        if(!empty($title)){
+            $final_title = $title; 
+        } else if(!empty($text)){
+            $final_title = $text;
+        } else {
+            $final_title = getCleanDomainAndPath($url);
+        }
+        
+        // GEFIXT: Das Auskommentieren/Überschreiben entfernt, damit der Titel nicht verloren geht!
+        $final_title = trim($final_title);
+        
+        $links_data[] = [ 'url' => esc_url($url), 'title' => esc_html($final_title) ];
+    }
+    usort($links_data, 'wpls_sort_sources');
+    $links_data = wpls_filter_unique_urls($links_data);
+
+    $images = $dom->getElementsByTagName('img');
+    foreach ($images as $img) {
+        $url = $img->getAttribute('src'); $alt = $img->getAttribute('alt'); $title = $img->getAttribute('title');
+        if (empty($url)) continue;
+        if (empty($alt) && empty($title)) {
+            $final_title = basename(parse_url($url, PHP_URL_PATH));
+            if (empty($final_title)) { $final_title = $url; }
+        } else { $final_title = !empty($alt) ? $alt : $title; }
+        $images_data[] = [ 'url' => esc_url($url), 'title' => esc_html($final_title) ];
+    }
+    usort($images_data, 'wpls_sort_sources');
+    $images_data = wpls_filter_unique_urls($images_data);
+
+    $tables = $dom->getElementsByTagName('table');
+    $table_count = 1;
+    foreach ($tables as $table) {
+        $t_title = ''; $anchor_id = $table->getAttribute('id'); $parent = $table->parentNode;
+        if (empty($anchor_id) && $parent && $parent->nodeName === 'figure') { $anchor_id = $parent->getAttribute('id'); }
+        if ($parent && $parent->nodeName === 'figure') {
+            $figcaptions = $parent->getElementsByTagName('figcaption');
+            if ($figcaptions->length > 0) { $t_title = trim($figcaptions->item(0)->textContent); }
+        }
+        if (empty($t_title)) {
+            $captions = $table->getElementsByTagName('caption');
+            if ($captions->length > 0) { $t_title = trim($captions->item(0)->textContent); }
+        }
+        if (empty($t_title)) { $t_title = sprintf(__('Table %d', 'wp-list-of-sources'), $table_count); }
+        $tables_data[] = [ 'title' => esc_html($t_title), 'anchor_id' => esc_attr($anchor_id) ];
+        $table_count++;
+    }
+    
+    $output = '<div class="' . $wrapper_class_str . '" style="margin-top: 30px; font-family: sans-serif;">';
+    $sections = [
+        ['title' => $title_sources, 'data' => $links_data, 'type' => 'links', 'empty' => __('Keine Links in diesem Beitrag gefunden.', 'wp-list-of-sources')],
+        ['title' => $title_images,  'data' => $images_data, 'type' => 'images', 'empty' => __('Keine Bilder in diesem Beitrag gefunden.', 'wp-list-of-sources')],
+        ['title' => $title_tables,  'data' => $tables_data, 'type' => 'tables', 'empty' => __('Keine Tabellen in diesem Beitrag gefunden.', 'wp-list-of-sources')]
+    ];
+    global $wpls_generated_anchors;
+
+    foreach ($sections as $section) {
+        $output .= sprintf('<%1$s class="wp-block-heading" style="margin-top:25px; margin-bottom:10px;">%2$s</%1$s>', $heading_tag, $section['title']);
+        if (!empty($section['data'])) {
+            if ($display_format === 'list') { $output .= '<ul class="wpls-sources-list" style="margin:0 0 20px 0; padding-left:20px;">'; }
+            else { $output .= sprintf('<table class="%s"><tbody>', $table_style_class); }
+
+            foreach ($section['data'] as $index => $item) {
+                $item_html = '';
+                if ($section['type'] === 'tables') {
+                    $final_anchor = (!empty($wpls_generated_anchors[$index])) ? $wpls_generated_anchors[$index] : (!empty($item['anchor_id']) ? $item['anchor_id'] : '');
+                    if (empty($final_anchor)) { $final_anchor = 'wpls-table-' . ($index + 1); }
+                    $item_html = sprintf('<a href="#%s">%s</a>', $final_anchor, $item['title']);
+                } else { $item_html = sprintf('<a href="%s" target="_blank" rel="noopener">%s</a>', $item['url'], $item['title']); }
+
+                if ($display_format === 'list') { $output .= sprintf('<li style="margin-bottom:5px;">%s</li>', $item_html); }
+                else { $output .= sprintf('<tr><td>%s</td></tr>', $item_html); }
+            }
+            if ($display_format === 'list') { $output .= '</ul>'; } else { $output .= '</tbody></table>'; }
+        } else { $output .= sprintf('<p style="font-style:italic; color:#888; margin-bottom:20px;">%s</p>', $section['empty']); }
+    }
+    $output .= '</div>';
+    set_transient($cache_key, $output, 12 * HOUR_IN_SECONDS);
+    return $output;
+}
+    $post_id = false;
+    if (defined('REST_REQUEST') && REST_REQUEST && !empty($_GET['post_id'])) { $post_id = intval($_GET['post_id']); }
+    if (!$post_id) { $post_id = get_the_ID(); }
+    $post = $post_id ? get_post($post_id) : null;
+    $is_template_preview = ( ! $post || $post->post_type === 'wp_block' || $post->post_type === 'wp_template' || $post_id === 0 );
+
+    $title_sources = !empty($attributes['titleSources']) ? esc_html($attributes['titleSources']) : 'Quellen';
+    $title_images  = !empty($attributes['titleImages']) ? esc_html($attributes['titleImages']) : 'Bilder';
+    $title_tables  = !empty($attributes['titleTables']) ? esc_html($attributes['titleTables']) : 'Tabellen';
+    $display_format = !empty($attributes['displayFormat']) ? $attributes['displayFormat'] : 'table';
+    $allowed_tags  = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'div'];
+    $heading_tag   = in_array($attributes['headingTag'], $allowed_tags) ? $attributes['headingTag'] : 'h3';
+    $table_style_class = ( !empty($attributes['className']) && strpos( $attributes['className'], 'is-style-stripes' ) !== false ) ? 'is-style-stripes' : '';
+
+    $wrapper_classes = [ 'wp-block-table', 'wp-block-sources-table' ];
+    if ( ! empty( $attributes['align'] ) ) $wrapper_classes[] = 'align' . $attributes['align'];
+    if ( ! empty( $attributes['className'] ) ) $wrapper_classes[] = $attributes['className'];
+    $wrapper_class_str = implode( ' ', array_map( 'sanitize_html_class', $wrapper_classes ) );
+
+    if ( $is_template_preview ) {
 		return wpls_render_template_dummy_preview($wrapper_class_str, $title_sources, $title_images, $title_tables, $heading_tag, $table_style_class, $display_format);
     }
 
@@ -155,7 +295,7 @@ function wpls_render_sources_table( $attributes, $content ) {
 		
 		// Choose the label for the link
 		if(!empty($title)){
-			$final_title = title;
+			$final_title = $title;
 		} else if(!empty($text)){
 			$final_title = $text;
 		} else {
